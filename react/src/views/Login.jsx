c53dcfb318e228d6855846240b7fb135f49ed0cf
Little Z <zy_mz17@hotmail.es>
2023-03-07 14:24:44 +0100

    scafolding

---
+++ react/src/views/Login.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+export default function Login() {
+    return (
+        <>
+            <h1>Hola</h1>
+            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+                Sign in to your account
+            </h2>
+        </>
+    )
+}
