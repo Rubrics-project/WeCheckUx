bbc4973bf876fb89c9f6bec90548abae6765f3c2
Crisktina <crishidalgolopez@gmail.com>
2023-03-08 12:31:00 +0100

    colors

--- react/tailwind.config.cjs
+++ react/tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  // plugins: [require("@tailwindcss/forms")],
+    content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                "color-bck": "#2f303a",
+                "color-bck": "#2f303a",
+                "color-bck": "#2f303a",
+            },
+        },
+    },
+    // plugins: [require("@tailwindcss/forms")],
 };
