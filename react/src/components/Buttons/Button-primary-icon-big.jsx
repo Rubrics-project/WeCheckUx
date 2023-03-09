696217d79f2b1953969f7a1b332f556107b824ed
Shima Naderi <shima.naderii@gmail.com>
2023-03-09 20:01:24 +0100

    buttons

---
+++ react/src/components/Buttons/Button-primary-icon-big.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ButtonSecondary({children}) {
+  return (
+    <button className= "font-bold py-2 px-4 rounded .w-20 bg-gray-100 .rounded   ">
+
+    {children}
+</button>
+  )
+}
--- react/src/views/Home.jsx
+++ react/src/views/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import ButtonMain from '../components/Buttons/ButtonMain'
 import Menu from '../components/menu'
 
 
-export default function Home() {
+export default function Home({children}) {
      
       return (
         <div>
           <Menu/>
+          <ButtonMain children={"Registrar"}/>
         </div>
       )
     
