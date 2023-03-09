00be6f5e23979493e7e0fed22a93c5e750fba8a4
mauisiri <irisleo.com@gmail.com>
2023-03-09 09:52:45 +0100

    all svg added

--- react/tailwind.config.cjs
+++ react/tailwind.config.cjs
@@ -13,9 +13,9 @@ module.exports = {
                 "color-grey-border": "#C8CCD1",
                 "color-grey-border-btn": "#A2A9B1",
 
-                "color-gradient-1": "linear-gradient(90deg, #0634AD 0%, #15D177 11.46%, #1D6C3D 23.44%, #B219F0 38.02%, #F4CD42 51.57%, #DE43A8 80.73%, #3B8F39 90.11%, #0634AD 100%)",
+                //"color-gradient-1": "linear-gradient(90deg, #0634AD 0%, #15D177 11.46%, #1D6C3D 23.44%, #B219F0 38.02%, #F4CD42 51.57%, #DE43A8 80.73%, #3B8F39 90.11%, #0634AD 100%)",
 
-                "color-gradient-2": "linear-gradient(90deg, #0645AD 3.65%, #FD9F12 21.35%, #FC4646 41.67%, #B219F0 65.1%, #24CA7A 79.17%, #1D6C3D 91.15%, #0645AD 100%)",
+                //"color-gradient-2": "linear-gradient(90deg, #0645AD 3.65%, #FD9F12 21.35%, #FC4646 41.67%, #B219F0 65.1%, #24CA7A 79.17%, #1D6C3D 91.15%, #0645AD 100%)",
             },
         },
     },
