c06927773d38790effce262b095fa9c9221608b8
Shima Naderi <shima.naderii@gmail.com>
2023-03-09 21:58:41 +0100

    data button

--- react/src/components/Buttons/ButtonSecondaryIconData.jsx
+++ react/src/components/Buttons/ButtonSecondaryIconData.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export default function ButtonSecondaryIconSmall({ text, onClick }) {
+export default function ButtonSecondaryIconData({ text, onClick }) {
   return (
     <button className= " font-opencustom text-color-grey-title font-bold bg-color-grey-bg  flex px-3 py-1 border-color-grey-border  border border-solid rounded-md ">
-        <img className='mr-2' src="../../public/assets/addIconGray.svg" alt="gray add icon" />
+    <img className='mr-2' src="../../public/assets/eyeIcon.svg" alt="eye icon" />
 
-    {text}
-   </button>
+{text}
+</button>
   )
 }
--- react/src/views/Home.jsx
+++ react/src/views/Home.jsx
@@ -3,6 +3,7 @@ import ButtonPrimary from "../components/buttons/ButtonPrimary";
 import ButtonPrimaryIcon from "../components/Buttons/ButtonPrimaryIcon";
 import ButtonPrimaryIconBig from "../components/Buttons/ButtonPrimaryIconBig";
 import ButtonSecondary from "../components/Buttons/ButtonSecondary";
+import ButtonSecondaryIconData from "../components/Buttons/ButtonSecondaryIconData";
 import ButtonSecondaryIconSmall from "../components/Buttons/ButtonSecondaryIconSmall";
 import Footer from "../components/Footer";
 import Menu from "../components/menu";
@@ -18,6 +19,8 @@ export default function Home() {
           <ButtonPrimaryIcon icon={"./public/assets/edit.svg"} text={"Editar"} onClick={"pasar onclick"} />
           <ButtonPrimaryIcon icon={"./public/assets/evaluate.svg"} text={"Evaluar"} onClick={"pasar onclick"} />
           <ButtonSecondaryIconSmall  text={"Añadir dimensión"} onClick={"pasar onclick"} />
+          <ButtonSecondaryIconData text={"Evaluaciones: 20"} onClick={"pasar onclick"} />
+          
 
           <Footer />
         </div>
