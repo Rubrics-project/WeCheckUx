cb746231b39f5085ac03939145ffdff616d8d245
rcarabal2022 <raquelcaraballospain@gmail.com>
2023-03-10 22:16:28 +0100

    Menú with functionality

--- react/src/components/Menu.jsx
+++ react/src/components/Menu.jsx
@@ -4,98 +4,100 @@ import projectsIcon from "../../public/assets/projectsIcon.svg";
 import addIcon from "../../public/assets/addIcon.svg";
 import accessIcon from "../../public/assets/accessIcon.svg";
 import gitHubIcon from "../../public/assets/gitHubIcon.svg";
-import logo from "../../public/assets/logo.svg";
+import { useState } from "react";
+
 
 export default function Menu() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
   return (
-    <div>
-      <div className="flex items-center justify-between border-b border-gray-400 py-8">
-        <a href="/">
-          <img src={logo} alt="Logo"></img>
-        </a>
-        <nav>
-          <section className="MOBILE-MENU flex lg:hidden">
-            <div className="HAMBURGER-ICON space-y-2">
-              <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
-              <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
-              <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+    <div className="flex items-center justify-between border-b border-gray-400 py-8">
+      <nav>
+        <section className="MOBILE-MENU flex lg:hidden">
+          <div
+            className="HAMBURGER-ICON space-y-2"
+            onClick={() => setIsNavOpen((prev) => !prev)}
+          >
+            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+          </div>
+          <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
+            <div
+              className="CROSS-ICON absolute top-0 right-0 px-8 py-8"
+              onClick={() => setIsNavOpen(false)}>
+              <svg
+                className="h-7 w-7 text-gray-600"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <line x1="18" y1="6" x2="6" y2="18" />
+                <line x1="6" y1="6" x2="18" y2="18" />
+              </svg>
             </div>
-
-            <div>
-              <div className="absolute top-0 right-0 px-8 py-8">
-                <svg
-                  className="h-8 w-8 text-gray-600"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <line x1="18" y1="6" x2="6" y2="18" />
-                  <line x1="6" y1="6" x2="18" y2="18" />
-                </svg>
-              </div>
-              <ul className="NAVIGATION-MOBILE-OPEN flex flex-col items-left justify-between min-h-[250px]">
-                <li className="border-b border-gray-400 my-2 uppercase">
-                  <div className="menuIcon">
-                    <img src={projectsIcon} alt="Icon Project"></img>
-                    <a href="/Projects">Proyectos</a>
-                  </div>
-                </li>
-                <li className="border-b border-gray-400 my-2 uppercase">
-                  <div className="menuIcon">
-                    <img src={addIcon} alt="Icon Create Rubric"></img>
-                    <a href="/Create"> Crear rúbricas</a>
-                  </div>
-                </li>
-                <li className="border-b border-gray-400 my-2 uppercase">
-                  <div className="menuIcon">
-                    <img src={faqIcon} alt="Icon Preguntas frecuentes"></img>
-                    <a href="/Fqa"> Preguntas frecuentes</a>
-                  </div>
-                </li>
-                <li className="border-b border-gray-400 my-2 uppercase">
-                  <div className="menuIcon">
-                    <img src={accessIcon} alt="Icon Login"></img>
-                    <a href="/Login">Acceder</a>
-                  </div>
-                </li>
-                <li className="border-b border-gray-400 my-2 uppercase">
-                  <div className="menuIcon">
-                    <img src={accessIcon} alt="Icon Login"></img>
-                    <a href="/Signup">Registro</a>
-                  </div>
-                </li>
-                <li className="border-b border-gray-400 my-2 uppercase">
-                  <div className="menuIcon">
-                    <img src={gitHubIcon} alt="Icon Login"></img>
-                    <a href="/Contibute">Constribuye al proyecto</a>
-                  </div>
-                </li>
-              </ul>
-            </div>
-          </section>
-
-          <ul className="DESKTOP-MENU hidden space-x-8 lg:flex">
-            <li>
-              <a href="/Projects">Proyectos</a>
-            </li>
-            <li>
-              <a href="/Create">Crear rúbricas</a>
-            </li>
-            <li>
-              <a href="/Fqa">Preguntas frecuentes</a>
-            </li>
-            <li>
-              <a href="/Login">Acceder</a>
-            </li>
-            <li>
-              <a href="/Signup">Registro</a>
-            </li>
+            <ul className="MENU-LINK-MOBILE-OPEN flex flex-col items-left justify-between min-h-[250px]">
+              <li className="border-b border-gray-400 my-2">
+                <div className="menuIcon">
+                  <img src={projectsIcon} alt="Icon Project"></img>
+                  <a href="/Proyectos">Proyectos</a>
+                </div>
+              </li>
+              <li className="border-b border-gray-400 my-2">
+                <div className="menuIcon">
+                  <img src={addIcon} alt="Icon Create Rubric"></img>
+                  <a href="/crear"> Crear rúbricas</a>
+                </div>
+              </li>
+              <li className="border-b border-gray-400 my-2">
+                <div className="menuIcon">
+                  <img src={faqIcon} alt="Icon Preguntas frecuentes"></img>
+                  <a href="/preguntasfrecuentes"> Preguntas frecuentes</a>
+                </div>
+              </li>
+              <li className="border-b border-gray-400 my-2">
+                <div className="menuIcon">
+                  <img src={accessIcon} alt="Icon Login"></img>
+                  <a href="/acceso">Acceder</a>
+                </div>
+              </li>
+              <li className="border-b border-gray-400 my-2">
+                <div className="menuIcon">
+                  <img src={accessIcon} alt="Icon Login"></img>
+                  <a href="/registro">Registro</a>
+                </div>
+              </li>
+              <li className="border-b border-gray-400 my-2">
+                <div className="menuIcon">
+                  <img src={gitHubIcon} alt="Icon Login"></img>
+                 <a href="https://github.com/Rubrics-project/WeCheckUx">Contribuye al proyecto</a>
+                </div>
+              </li>
             </ul>
-        </nav>
-      </div>
+          </div>
+        </section>
+      </nav>
+      <style>{`
+      .hideMenuNav {
+        display: none;
+      }
+      .showMenuNav {
+        display: block;
+        position: absolute;
+        width: auto;
+        height: 60vh;
+        top: 0;
+        left: 0;
+        background: white;
+        z-index: 10;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        align-items: left;
+      }
+    `}</style>
     </div>
   );
 }
