b4009396217f0dd7ea282c1db50493740216eaa6
Little Z <zy_mz17@hotmail.es>
2023-03-10 09:11:46 +0100

    component accordion in faq

--- react/src/views/Fqa.jsx
+++ react/src/views/Fqa.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Accordion from '../components/Accordion'
 
 export default function Fqa() {
   return (
-    <div>Fqa</div>
+    <div>
+      <Accordion title="Accordion 1" content="Content for Accordion 1" />
+      <Accordion title="Accordion 2" content="Content for Accordion 2" />
+      <Accordion title="Accordion 3" content="Content for Accordion 3"/>
+    </div>
   )
 }
