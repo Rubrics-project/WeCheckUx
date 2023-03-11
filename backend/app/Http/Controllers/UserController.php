72a73bcdc169fa9a73832e6f548b990b0d034dcc
Little Z <zy_mz17@hotmail.es>
2023-03-11 14:27:16 +0100

    api user done

---
+++ backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return User::all();
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name'=>'required',
+            'email'=>'required',
+            'password'=>'required',
+
+        ]);
+       $user = new User;
+       $user->name =  $request->name;
+       $user->email = $request->email;
+       $user->password = $request->password;
+
+       $user->save();
+       return $user;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        return $user;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, User $user)
+    {
+        $request->validate([
+            'name'=>'required',
+            'email'=>'required',
+            'password'=>'required',
+
+        ]);
+
+       $user->name =  $request->name;
+       $user->email = $request->email;
+       $user->password = $request->password;
+
+       $user->update();
+       return $user;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        $user = User::find($id);
+        if(is_null($user)){
+            return response()->json("Usuario no encontrado", 404);
+        }
+        $user->delete();
+        return response()->json("Usiario eliminado con exito!", 204);
+    }
+}
