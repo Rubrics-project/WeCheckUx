d795dc3f921f9041756677b777dabcd0f092063d
Shima Naderi <shima.naderii@gmail.com>
2023-03-13 09:50:16 +0100

    changes

--- react/src/views/Create.jsx
+++ react/src/views/Create.jsx
@@ -2,25 +2,22 @@ import React from 'react'
 
 export default function Create() {
   return (
-    
-        <div>Buscador
-
+    <>
+        
           <h1>Elegir proyecto</h1>
           <p>Selecciona el proyecto en el que quieras crear tu rúbrica.</p>
           <line x1="18" y1="6" x2="6" y2="18" />
-          <section>
+          
             <form action="/create" method="post" id="create">
             <label for="name">Tutilo:</label>
-            <input type="text" id="name" name="name"><br>
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email"><br>
+            <input type="text" id="tutilo" name="tutilo"> </input><br>
+            <label for="email">Descripción de la rúbrica::</label>
+            <input type="Problemas de acceso basados en los roles  e identidad de género." id="description" name="description"> </input><br>
 
             <button type="submit">Submit</button>
-          </form> 
-          </section>
-
-
-    </div>
-    
-  )
+            </form>
+          
+         
+    </>
+  );
 }
