2f6efb92878197b2a21eb0c3ad1374b97470c049
Shima Naderi <shima.naderii@gmail.com>
2023-03-13 11:45:19 +0100

    accept

--- react/src/views/Create.jsx
+++ react/src/views/Create.jsx
@@ -9,12 +9,24 @@ export default function Create() {
           <line x1="18" y1="6" x2="6" y2="18" />
           
             <form action="/create" method="post" id="create">
-            <label for="name">Tutilo:</label>
-            <input type="text" id="tutilo" name="tutilo"> </input><br>
-            <label for="email">Descripción de la rúbrica::</label>
-            <input type="Problemas de acceso basados en los roles  e identidad de género." id="description" name="description"> </input><br>
+            <label for="title">Título:</label>
+            <input type="text" id="title" name="title" placeholder='Título'> </input>
 
-            <button type="submit">Submit</button>
+            <label for="description">Descripción de la rúbrica:</label>
+            <input type="text" id="description" name="description" placeholder='Problemas de acceso basados en los roles  e identidad de género.'> </input>
+
+            <label for="dimension">Dimensión:</label>
+            <input type="text" id="dimension" name="dimension" placeholder='Dimensión'> </input>
+
+            <label for="description">Descripción de la dimensión:</label>
+            <input type="text" id="description" name="description" placeholder='Descripción'> </input>
+
+            <lable for="evaluation" > Criterio a evaluar:</lable>
+            <input type="text" id="evaluation" name="evaluation" placeholder='Criterio que ha de ser evaluado'> </input>
+
+
+
+            <button type="submit">Añadir dimensión</button>
             </form>
           
          
