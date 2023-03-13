97da733372d5786f291ad842d2bcbcaeae21420a
rcarabal2022 <raquelcaraballospain@gmail.com>
2023-03-13 21:35:08 +0100

    Header almost ready

--- react/src/components/Header.jsx
+++ react/src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Menu from "./menu/Menu";
+import Nav from "../components/Nav";
 
 export default function Header() {
   return (
     <>
       <Menu />
+      <Nav />
     </>
   );
 }
--- react/src/main.jsx
+++ react/src/main.jsx
@@ -4,6 +4,7 @@ import { RouterProvider } from "react-router-dom";
 // import { ContextProvider } from "./contexts/ContextProvider";
 import "./index.css";
 import router from "./router/router";
+import "./components/menu/menu.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
