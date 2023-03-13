98e45a8d8b67485de1b4330f95fcad82c7b8fa78
Little Z <zy_mz17@hotmail.es>
2023-03-13 13:06:12 +0100

    userservice done

--- react/src/views/Rubrics.jsx
+++ react/src/views/Rubrics.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+// import { getAllItems } from '../services/userService.js';
+
 
 export default function Rubrics() {
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        getAllItems()
+          .then(response => {
+            setItems(response.data);
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      }, []);
     return (
         <div>
             Rubrics
-            <div></div>
+            {items.map(item => (
+            <div key={item.id}>{item.name}</div>
+            ))}
         </div>
     );
 }
