d4606ef3f7777b06856face91ae76f414f839beb
mauisiri <irisleo.com@gmail.com>
2023-03-14 12:54:09 +0100

    merge with dev header

--- react/src/components/Header.jsx
+++ react/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Menu from "./menu/Menu";
 import UserMenu from "./userMenu/UserMenu";
+import logo from "../assets/logo.svg";
 
 export default function Header() {
   return (
