0b5086c303ddc2128bc5341726fa1475dbbd6e7d
Little Z <zy_mz17@hotmail.es>
2023-03-16 00:43:15 +0100

    funtionality about token y login done

--- react/src/components/Header.jsx
+++ react/src/components/Header.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Menu from "./menu/Menu";
 import UserMenu from "./userMenu/UserMenu";
 import logo from "../assets/logo.svg";
+import Nav from "./Nav";
 
 export default function Header() {
   return (
     <div className="flex  justify-between  p-6 bg-color-grey-bg">
-         <Menu />
+         {/* <Menu /> */}
+         <Nav/>
        <a href="/"><img src={logo} alt="Icon Create Rubric"></img></a>
-      <UserMenu />
+      <Menu />
     </div>
   );
 }
--- react/src/components/Nav.jsx
+++ react/src/components/Nav.jsx
@@ -1,56 +1,66 @@
-import React from "react";
-import logo from "../assets/logo.svg";
-import loggedIcon from "../assets/loggedIcon.svg";
-import Title from "./Title";
+import React, { useEffect, useState } from 'react';
+import faqIcon from "../assets/faqIcon.svg";
+import projectsIcon from "../assets/projectsIcon.svg";
+import addIcon from "../assets/addIcon.svg";
+import accessIcon from "../assets/accessIcon.svg";
+import gitHubIcon from "../assets/gitHubIcon.svg";
+import logoutIcon from "../assets/logoutIcon.svg";
+
+import menuIcon from '../assets/hamburger.svg'
+
+
 
 export default function Nav() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      setIsAuthenticated(true);
+    }
+  }, []);
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    setIsAuthenticated(false);
+    // redirigir a la página de inicio de sesión o a la página principal
+  };
+
+
+
   return (
-    <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 flex flex-wrap justify-between items-center dark:bg-gray-800">
-      <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-        <a href="/">
-          <img src={logo} alt="Logo"></img>
-        </a>
+
+    <div>
+      <img
+            src={menuIcon}
+            alt="Menu Hamburgesa"
+            onClick={() => setIsNavOpen((prev) => !prev)}
+          ></img>
+      <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
+
+          {isAuthenticated ? (
+            <>
+              <a href="/proyectos" className='flex'><img src={projectsIcon} alt="Icon Project"></img>Proyectos</a>
+              <a href="/rubricas" className='flex'><img src={projectsIcon} alt="Icon Project"/>Mis Rúbricas</a>
+              <a href="/evaluar" className='flex'><img src={projectsIcon} alt="Icon Project"/>Mis Evaluaciones</a>
+              <a href="/crear" className='flex'><img src={addIcon} alt="Icon Create Rubric"/>Crear Rúbrica</a>
+              <a href="/preguntasfrecuentes" className='flex'><img src={faqIcon} alt="Icon Preguntas frecuentes"></img>Preguntas Frecuentes</a>
+              <button onClick={handleLogout}><img src={logoutIcon} alt="Icon Logout"/>Cerrar sesión</button>
+            </>
+          ) : (
+            <>
+              <a href="/proyectos" className='flex'><img src={projectsIcon} alt="Icon Project"/>Proyectos</a>
+              <a href="/preguntasfrecuentes" className='flex'><img src={faqIcon} alt="Icon Preguntas frecuentes"/>Preguntas Frecuentes</a>
+              <a href="/acceso" className='flex'><img src={accessIcon} alt="Icon Login"/>Iniciar sesión</a>
+              <a href="/registro" className='flex'><img src={accessIcon} alt="Icon Login"/>Registrarse</a>
+            </>
+          )}
+          <a href="https://github.com/Rubrics-project/WeCheckUx" className='flex'><img src={gitHubIcon} alt="Icon Login"/>Contribuye al proyecto</a>
       </div>
-      <div class="flex items-center lg:order-2">
-        <a
-          href="#"
-          class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
-        >
-          <img src={loggedIcon} alt="PerfilUsuario"></img>
-        </a>
-      </div>
-      <div
-        class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
-        id="mobile-menu"
-      >
-        <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-          <li>
-            <a href="/proyectos">
-              <Title title={"Proyectos"} />
-            </a>
-          </li>
-          <li>
-            <a href="/crear">
-              <Title title={"Crear rúbricas"} />
-            </a>
-          </li>
-          <li>
-            <a href="/preguntasfrecuentes">
-              <Title title={"Preguntas frecuentes"} />
-            </a>
-          </li>
-          <li>
-            <a href="/acceso">
-              <Title title={"Acceder"} />
-            </a>
-          </li>
-          <li>
-            <a href="/registro">
-              <Title title={"Registro"} />
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
+
+
+    </div>
+    // </Menu>
+  )
 }
+
