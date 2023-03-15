4581e009651fc3d932fde8341f24ccb90547fa2b
mauisiri <irisleo.com@gmail.com>
2023-03-15 08:52:35 +0100

    browser

--- react/src/components/Browser.jsx
+++ react/src/components/Browser.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
 import browserIcon from "../assets/browserIcon.svg";
-
+import { useState } from 'react';
+import { createAutocomplete } from '@algolia/autocomplete-core'
 
 export default function Browser() {
+  const[autocompleteState, setAutocompleteState] = useState({
+      collections: [],
+      isOpen: false
+  })
+
+  const autocomplete = createAutocomplete({
+    onStateChange: ({ state }) => setAutocompleteState(state),
+    getSources: () => [{
+      souceId: 'offers-next-api',
+      getItems: ({ query }) => {
+        if (!!query) {
+          return fetch (`/api/search?q=´${query}`)
+            .then(res => res.json())
+        }
+
+      }
+    }]
+  })
+  
   return (
     <form className= 'flex justify-center mb-50'>
         <div className='flex relative p-0.5 bg-color-grey-title rounded w-2/6'>
