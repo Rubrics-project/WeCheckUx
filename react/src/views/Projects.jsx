12aa2aea9ee18b76f28005fb24ab5c5b933faea9
mauisiri <irisleo.com@gmail.com>
2023-03-16 14:06:48 +0100

    componente y view project

--- react/src/views/Projects.jsx
+++ react/src/views/Projects.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Browser from "../components/Browser";
+import Card from "../components/projects/Card";
 import Title from "../components/Title";
 
 export default function Projects() {
@@ -7,6 +8,16 @@ export default function Projects() {
     <>
       <Browser />
       <Title title={"Proyectos"} />
+      {/* HACER BUCLE PARA TRAER TODOS LOS ID DE RUBRICAS DE EL PROYECTO SELECCIONADO */}
+      {/* Añadir botones Evaluar y evaluaciones */}
+      <Card
+        title={"Título proyecto"}
+        rubric_description={
+          "Problemas de acceso basados en los roles e identidad de género."
+        }
+        rubric_author={"Juana Arcos"}
+        rubric_date={"20/3/2022"}
+      />
     </>
   );
 }
