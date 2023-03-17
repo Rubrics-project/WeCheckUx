11b92a689219655fe70bb80a7b88be5499f63f5c
Little Z <zy_mz17@hotmail.es>
2023-03-17 11:02:38 +0100

    merge create

--- react/src/components/Buttons/ButtonSecondaryIconSmall.jsx
+++ react/src/components/Buttons/ButtonSecondaryIconSmall.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
-export default function ButtonSecondaryIconSmall({ text, onClick }) {
+export default function ButtonSecondaryIconSmall({ text, src }) {
   return (
-    <button className= " font-opencustom text-color-grey-title font-bold bg-color-grey-bg  flex px-3 py-1 border-color-grey-border  border border-solid rounded-md ">
-        <img className='mr-2' src="../../assets/addIconGray.svg" alt="gray add icon" />
+    <div>
 
-    {text}
-   </button>
+      <img className='mr-2' src={src} />
+      <button className=" font-opencustom text-color-grey-title font-bold bg-color-grey-bg  flex px-3 py-1 border-color-grey-border  border border-solid rounded-md ">
+
+        {text}
+      </button>
+    </div>
   )
 }
--- react/src/components/InformationBox.jsx
+++ react/src/components/InformationBox.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import infoIcon from "../assets/infoCircle.svg"
 
 export default function InformationBox({text}) {
   return (
     <div>
-      <img className='mr-2' src="../../assets/infoCircle.svg" alt="information icon" />
+      <img className='mr-2' src={infoIcon} alt="information icon" />
       <p>{text}</p>
     </div>
   )
