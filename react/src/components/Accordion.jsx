1b7cf2b96cbd75617fd6406b4f96487af15a1035
Crisktina <crishidalgolopez@gmail.com>
2023-03-22 13:45:28 +0100

    preguntas con texto y estilos

--- react/src/components/Accordion.jsx
+++ react/src/components/Accordion.jsx
@@ -8,15 +8,28 @@ const Accordion = ({ title, content }) => {
   };
 
   return (
-    <div className="border border-gray-300 rounded-md">
-      <div className="flex items-center justify-between p-4 cursor-pointer" onClick={toggleAccordion}>
-        <h3 className="text-lg font-medium">{title}</h3>
-        <svg className={`w-6 h-6 ${isOpen ? 'transform rotate-180' : ''}`} fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
+    <div className="border border-color-grey-border rounded-md overflow-hidden">
+      <div
+        className="flex items-center justify-between p-4 cursor-pointer bg-color-grey-bg"
+        onClick={toggleAccordion}
+      >
+        <h3 className="font-opencustom text-xs font-bold ">{title}</h3>
+        <svg
+          className={`w-6 h-6 ${isOpen ? "transform rotate-180" : ""}`}
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M19 9l-7 7-7-7"
+          />
         </svg>
       </div>
       {isOpen && (
-        <div className="p-4 border-t border-gray-300">
+        <div className="font-opencustom text-xs p-4 border-t border-color-grey-border">
           <p>{content}</p>
         </div>
       )}
