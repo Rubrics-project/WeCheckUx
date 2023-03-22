dc91bdb5add1dcd05be6b334a8a6a50de5a6fdaa
Crisktina <crishidalgolopez@gmail.com>
2023-03-22 16:31:43 +0100

    render data ok en rubric complete

--- react/src/views/RubricDetail.jsx
+++ react/src/views/RubricDetail.jsx
@@ -6,12 +6,17 @@ import { getItemById } from "../services/rubricService";
 export default function RubricDetail() {
   const params = useParams();
   const [rubric, setRubric] = useState([]);
+  const [user, setUser] = useState([]);
+  const [project, setProject] = useState([]);
+  const [date, setDate] = useState([]);
 
   useEffect(() => {
     getItemById(params.id)
       .then((response) => {
         setRubric(response.rubric);
-
+        setUser(response.rubric.user);
+        setProject(response.rubric.project);
+        setDate(response.rubric.created_at.slice(0, 10));
         console.log("setRubrics rubric detail: ", response.rubric);
       })
       .catch((error) => {
@@ -24,11 +29,11 @@ export default function RubricDetail() {
       <RubricComplete
         rubric_title={rubric.title}
         rubric_description={rubric.description}
-        rubric_author={rubric.user_id}
-        rubric_date={rubric.created_at}
-        project_title={rubric.project_id}
-        project_url={rubric.project_id}
-        project_description={rubric.project_id}
+        rubric_author={user.name + " " + user.surname}
+        rubric_date={date}
+        project_title={project.name}
+        project_url={project.url}
+        project_description={project.description}
       />
     </>
   );
