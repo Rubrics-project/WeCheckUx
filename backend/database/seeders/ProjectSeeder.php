38e46c71c866a41253e18454b45ddb801500cf05
Crisktina <crishidalgolopez@gmail.com>
2023-03-22 20:05:30 +0100

    seeder proyectos actualizado con datos reales

--- backend/database/seeders/ProjectSeeder.php
+++ backend/database/seeders/ProjectSeeder.php
@@ -13,6 +13,31 @@ class ProjectSeeder extends Seeder
      */
     public function run(): void
     {
-        Project::factory()->count(10)->create();
+        // Project::factory()->count(10)->create();
+
+        Project::insert([
+            [
+              'name' => 'Página de inicio de la web UOC.edu',
+              'url' => 'www.uoc.edu',
+              'description' => 'La página de entrada de la Universidad Oberta de Catalunya es la puerta de acceso para sus estudiantes y el portal de difusión y publicidad de la universidad y sitio de acceso a los futuros estudiantes para obtener información.',
+              'created_at' => now(),
+              'updated_at' => now(),
+            ],
+            [
+                'name' => 'Página de inicio de la web UOC.edu',
+                'url' => 'www.uoc.edu',
+                'description' => 'La página de entrada de la Universidad Oberta de Catalunya es la puerta de acceso para sus estudiantes y el portal de difusión y publicidad de la universidad y sitio de acceso a los futuros estudiantes para obtener información.',
+                'created_at' => now(),
+                'updated_at' => now(),
+              ],
+              [
+                'name' => 'Aula antigua de la Universitat Oberta de Catalunya',
+                'url' => '#Acceso restringido a miembros de la universidad',
+                'description' => 'El aula antigua es el espacio dónde se imparte la docencia y se despliega el proceso de aprendizaje de la universidad online.',
+                'created_at' => now(),
+                'updated_at' => now(),
+              ],
+            
+          ]);
     }
 }
--- react/src/views/Home.jsx
+++ react/src/views/Home.jsx
@@ -14,11 +14,11 @@ export default function Home() {
   return (
     <>
       <Browser />
-      <h1 className="font-opencustom font-bold text-2xl py-14">
+      <h1 className="font-latocustom  font-semibold text-2xl pt-11 pb-14">
         ¡Te damos la bienvenida a WeCheck!
       </h1>
 
-      <p className="font-opencustom text-sm">
+      <p className="font-latocustom text-sm">
         Esto es una plataforma de usuarios para{" "}
         <b>evaluar distintas plataformas digitales mediante rúbricas</b>. Las
         rúbricas son instrumentos de análisis cualitativo que nos permiten
@@ -28,7 +28,7 @@ export default function Home() {
         gráficas de usuario en nuestro modo de relacionarnos como sociedad.
       </p>
 
-      <p className="font-opencustom text-sm py-10">
+      <p className="font-latocustom text-sm py-10">
         En WeCheck puedes evaluar, mediante rúbricas creadas por diferentes
         autores y autoras, la interfaz de cualquier espacio. Además, tienes la
         posibilidad de <b>crear tus propias rúbricas</b> para ponerlas a
@@ -38,25 +38,25 @@ export default function Home() {
         <b>tu contribución es muy valiosa</b>.
       </p>
 
-      <p className="font-opencustom text-sm">
+      <p className="font-latocustom text-sm">
         Si tienes alguna duda, consulta nuestras <br />{" "}
         <span>
           {" "}
           <Link
             to="/preguntasfrecuentes"
-            className="font-opencustom text-sm text-color-blue-p underline hover:text-color-blue-light"
+            className="font-latocustom text-sm text-color-blue-p underline hover:text-color-blue-light"
           >
             Preguntas frecuentes.
           </Link>
         </span>
       </p>
 
-      <p className="font-opencustom text-sm font-bold py-12">
+      <p className="font-latocustom text-sm font-bold py-12">
         ¡Exprésate libremente y contribuye a la mejora y evaluación de las
         interfaces gráficas!
       </p>
       {!isAuthenticated ? (
-        <div className="w-full grid grid-cols-2 gap-7 mb-20 ">
+        <div className="w-full grid grid-cols-2 gap-7 mb-5 ">
           <Link
             to="/acceso"
             className="flex justify-center font-opencustom text-color-blue-p font-bold bg-color-grey-bg px-4 py-2 border-color-grey-border border border-solid rounded-md "
@@ -74,7 +74,7 @@ export default function Home() {
         <div>
           <Link
             to="/crear"
-            className="flex justify-center font-opencustom text-color-blue-p font-bold bg-color-grey-bg px-4 py-2 border-color-grey-border border border-solid rounded-md mb-20 "
+            className="flex justify-center font-opencustom text-color-blue-p font-bold bg-color-grey-bg px-4 py-2 border-color-grey-border border border-solid rounded-md mb-5 "
           >
             Crear rúbrica
           </Link>
