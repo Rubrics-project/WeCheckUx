ebc7bc1a740017f8cfc9080a9112b9736374d376
Shima Naderi <shima.naderii@gmail.com>
2023-03-28 18:38:43 +0200

    test cyperess

--- react/cypress.config.js
+++ react/cypress.config.js
@@ -2,14 +2,12 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   testFiles: "**/*.{spec,test}.js",
-  integrationFolder: "cypress/components",
   devServer: {
     command: "npm run start",
     port: 3000,
     host: "localhost",
   },
-
   e2e: {
     specPattern: 'cypress/components/**/*.spec.{js,ts,jsx,tsx}'
   },
-  });
+});
--- react/cypress/component/Browser.spec.js
+++ react/cypress/component/Browser.spec.js
@@ -2,19 +2,14 @@ import Browser from "../../src/components/Browser";
 
 describe("Browser component", () => {
   it("renders correctly", () => {
-    // Mount the component using the mount command from Cypress React Selector
     cy.mount(<Browser />);
     
-    // Check that the form is rendered on the page
     cy.get("form").should("exist");
 
-    // Type a search term into the input field and check that it updates correctly
     cy.get("input").type("example search term").should("have.value", "example search term");
 
-    // Click the browser icon button and check that it is clickable
     cy.get("button").should("be.visible").click();
 
-    // Check that the correct image is rendered inside the button
     cy.get("button img").should("have.attr", "src", "/path/to/browserIcon.svg");
   });
 });
