bb6e615f5500906d8df78d6f5077e1e659d1c942
rcarabal2022 <raquelcaraballospain@gmail.com>
2023-03-28 12:44:39 +0200

    Install cypress and begin test

---
+++ react/cypress/component/Footer.cy.jsx
@@ -0,0 +1,21 @@
+import Footer from "../../src/components/Footer"
+
+
+
+describe('Accordion.cy.jsx', () => {
+  it('test go to github', () => {
+    cy.mount(<Footer />);
+    cy.get("a").click();
+  })
+})
+
+// cy.origin('https://github.com', () => {
+// cy.on('uncaught:exception', (e) => {
+// if (e.message.includes('Things went bad')) {
+// // we expected this error, so let's ignore it
+// // and let the test continue
+// return false
+// }
+// })
+// })
+// cy.visit('https://github.com/Rubrics-project/WeCheckUx')
