cfdf4d584b2be4025db448e694bcb7ebb14bb4a3
Crisktina <crishidalgolopez@gmail.com>
2023-03-29 14:44:20 +0200

    alertas implementadas

--- react/src/components/Buttons/ButtonSecondary.jsx
+++ react/src/components/Buttons/ButtonSecondary.jsx
@@ -3,6 +3,7 @@ import React from "react";
 export default function ButtonSecondary({ text, onClick }) {
   return (
     <button
+      type="button"
       className="flex justify-center items-center align-middle font-opencustom text-color-grey-title font-bold bg-color-grey-bg   px-12 py-2 border-color-grey-border  border border-solid rounded-md "
       onClick={onClick}
     >
--- react/src/views/Create.jsx
+++ react/src/views/Create.jsx
@@ -14,7 +14,7 @@ import CreateRubricForm from "../components/createRubrics/CreateRubricForm";
 import CreateButtonsDimension from "../components/createRubrics/CreateButtonsDimension";
 import Spinner from "../components/Spinner";
 import Swal from "sweetalert2";
-
+import checkBig from "../assets/doubleCheckBig.svg";
 
 export default function Create() {
   const { currentUser } = userAuthContext();
@@ -27,17 +27,15 @@ export default function Create() {
   const [title, setRubricTitle] = useState("");
   const [description, setRubricDescription] = useState("");
   const [evaluationDimensionTitle, setEvaluationDimensionTitle] = useState("");
-  const [evaluationDimensionDescription, setEvaluationDimensionDescription] = useState("");
+  const [evaluationDimensionDescription, setEvaluationDimensionDescription] =
+    useState("");
   const [evaluation_text, setEvaluationText] = useState("");
   const [negative, setNegative] = useState("");
   const [regular, setRegular] = useState("");
   const [suficient, setSuficient] = useState("");
   const [good, setGood] = useState("");
   const [excelent, setExcelent] = useState("");
-  // const [idRubric, setidRubric] = useState();
   const [isLoading, setIsLoading] = useState(true);
-  // const [showButton, setShowButton] = useState(false);
-
 
   if (!currentUser) {
     return <Navigate to="/acceso" />;
@@ -62,9 +60,7 @@ export default function Create() {
 
     fetchData();
   }, []);
-  // const handleButtonClick = () => {
-  //   setShowButton(true);
-  // };
+
   const handleRubricTitleChange = (e) => {
     setRubricTitle(e.target.value);
   };
@@ -126,27 +122,17 @@ export default function Create() {
         formDataEvaluation
       );
       console.log(responseCreateEvaluation);
+      Swal.fire({
+        title: `<div className="flex flex-row justify-center bg-color-grey-bg pb-10" style="display: flex;justify-content: center;padding-bottom: 20px;"><img className="flex" src="${checkBig}" alt="Icono doble check" /><h3 className="flex font-opencustom text-xl font-bold text-color-bck">ALERTA</h3></div>`,
+        text: "¡Tu rúbrica ha sido creada con éxito!",
+        showConfirmButton: false,
+        timer: 2000,
+      });
       window.location.href = "/mis-rubricas";
     } catch (err) {
       console.log(JSON.parse(err.request.response).msg);
     }
   };
-
-  // const handleCancelRubric = useCallback(async () => {
-  //   try {
-  //     // setDeleted(true);
-  //     await Swal.fire({
-  //       title: `<div className="flex flex-row justify-center bg-color-grey-bg pb-10" style="display: flex;justify-content: center;padding-bottom: 20px;"><img className="flex" src="${checkBig}" alt="Icono doble check" /><h3 className="flex font-opencustom text-xl font-bold text-color-bck">ALERTA</h3></div>`,
-  //       text: "La rúbrica ha sido cancelada.",
-  //       showConfirmButton: false,
-  //       timer: 1000,
-  //     });
-  //     window.location.href = "/proyectos";
-  //   } catch (error) {
-  //     console.error(error);
-  //     Swal.fire("¡Error!", "No se pudo eliminar la rúbrica.", "error");
-  //   }
-  // }, []);
 
   const handleConfirmCancel = () => {
     Swal.fire({
@@ -161,97 +147,100 @@ export default function Create() {
         "<span style='color: #545454;font-weight: 400;'>No</span>",
     }).then((result) => {
       if (result.isConfirmed) {
-        // handleCancelRubric();
         Swal.fire({
           title: `<div className="flex flex-row justify-center bg-color-grey-bg pb-10" style="display: flex;justify-content: center;padding-bottom: 20px;"><img className="flex" src="${checkBig}" alt="Icono doble check" /><h3 className="flex font-opencustom text-xl font-bold text-color-bck">ALERTA</h3></div>`,
           text: "La rúbrica ha sido cancelada.",
           showConfirmButton: false,
           timer: 1000,
         }).then(() => {
-          setTimeout(() => {
-            window.location.href = "/proyectos";
-          }, 2000);
+          window.location.href = "/proyectos";
         });
       }
     });
-  }
+  };
 
-    return (
-      <>
-        {isLoading ? (
+  return (
+    <>
+      {isLoading ? (
+        <div className="flex justify-center mt-14">
+          <Spinner />
+        </div>
+      ) : (
+        <>
+          <Title title={project.name} />
 
-          <div className="flex justify-center mt-14">
-            <Spinner />
-          </div>
-        ) : (
-          <>
-            <Title title={project.name} />
-
-            <ProjectHeaderDetail
-              project_url={project.url}
-              project_description={project.description}
+          <ProjectHeaderDetail
+            project_url={project.url}
+            project_description={project.description}
+          />
+          <div className="my-5">
+            <InformationBox
+              text={
+                "Crea una rúbrica para que tú u otras personas la podáis usar para evaluar la experiencia de usuario de una web."
+              }
             />
-            <div className="my-5">
-              <InformationBox
-                text={
-                  "Crea una rúbrica para que tú u otras personas la podáis usar para evaluar la experiencia de usuario de una web."
-                }
+          </div>
+          <form
+            onSubmit={onSubmit}
+            action="#"
+            method="POST"
+            className="space-y-1"
+          >
+            <div className="border rounded border-color-blue-p mb-7 overflow-hidden">
+              <input
+                className="hidden"
+                type="number"
+                name="project_id"
+                value={project_id}
+                readOnly
+              />
+              <input
+                className="hidden"
+                type="number"
+                name="user_id"
+                value={user_id}
+                readOnly
+              />
+              <CreateRubricForm
+                title_value={title}
+                title_onChange={handleRubricTitleChange}
+                description_value={description}
+                description_onChange={handleRubricDescriptionChange}
+              />
+              <DimensionForm
+                title_value={evaluationDimensionTitle}
+                title_onChange={handleDimensionTitleChange}
+                description_value={evaluationDimensionDescription}
+                description_onChange={handleDimensionDescriptionChange}
+                evaluation_text_value={evaluation_text}
+                evaluation_text_onChange={handleEvaluationTextChange}
+                negative_value={negative}
+                negative_onChange={handleNegativeChange}
+                regular_value={regular}
+                regular_onChange={handleRegularChange}
+                suficient_value={suficient}
+                suficient_onChange={handleSuficientChange}
+                good_value={good}
+                good_onChange={handleGoodChange}
+                excelent_value={excelent}
+                excelent_onChange={handleExcelentChange}
+              />
+
+              <CreateButtonsDimension
+              //   onClickAddDimension={"TODO: funcion para añadir dimesion"}
+              //  onClickDeleteDimension={"TODO: funcion para eliminar dimesion"}
               />
             </div>
-            <form onSubmit={onSubmit} action="#" method="POST" className="space-y-1">
-              <div className="border rounded border-color-blue-p mb-7 overflow-hidden">
-                <input
-                  className="hidden"
-                  type="number"
-                  name="project_id"
-                  value={project_id}
-                  readOnly
-                />
-                <input
-                  className="hidden"
-                  type="number"
-                  name="user_id"
-                  value={user_id}
-                  readOnly
-                />
-                <CreateRubricForm
-                  title_value={title}
-                  title_onChange={handleRubricTitleChange}
-                  description_value={description}
-                  description_onChange={handleRubricDescriptionChange}
-                />
-                <DimensionForm
-                  title_value={evaluationDimensionTitle}
-                  title_onChange={handleDimensionTitleChange}
-                  description_value={evaluationDimensionDescription}
-                  description_onChange={handleDimensionDescriptionChange}
-
-                  evaluation_text_value={evaluation_text}
-                  evaluation_text_onChange={handleEvaluationTextChange}
-                  negative_value={negative}
-                  negative_onChange={handleNegativeChange}
-                  regular_value={regular}
-                  regular_onChange={handleRegularChange}
-                  suficient_value={suficient}
-                  suficient_onChange={handleSuficientChange}
-                  good_value={good}
-                  good_onChange={handleGoodChange}
-                  excelent_value={excelent}
-                  excelent_onChange={handleExcelentChange}
-                />
-
-                <CreateButtonsDimension
-                //   onClickAddDimension={"TODO: funcion para añadir dimesion"}
-                //  onClickDeleteDimension={"TODO: funcion para eliminar dimesion"}
-                />
-              </div>
-              <div className="grid w-full grid-cols-2 gap-7">
-                {/*TODO: boton de type="submit" luego tiene que llevarte a mis-rubricas */}
-                <ButtonPrimary text={"Guardar"} />
-                <ButtonSecondary text={"Cancelar"} onClick={handleConfirmCancel} />
-              </div>
-            </form>
-          </>)}
-      </>
-    );
-  }
+            <div className="grid w-full grid-cols-2 gap-7">
+              <ButtonPrimary text={"Guardar"} />
+              <ButtonSecondary
+                text={"Cancelar"}
+                onClick={handleConfirmCancel}
+              />
+            </div>
+          </form>
+        </>
+      )}
+    </>
+  );
+}
