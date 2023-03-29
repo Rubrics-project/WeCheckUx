48142f959ffc51354c1f2e8901a9e0aca10b4a18
Shima Naderi <shima.naderii@gmail.com>
2023-03-29 11:32:42 +0200

    accept

--- react/cypress.config.js
+++ react/cypress.config.js
@@ -1,16 +1,3 @@
-const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
-  testFiles: "**/*.{spec,test}.js",
-  devServer: {
-    command: "npm run start",
-    port: 3000,
-    host: "localhost",
-  },
-  e2e: {
-    specPattern: 'cypress/components/**/*.spec.{js,ts,jsx,tsx}'
-  },
-});
 
 const { defineConfig } = require("cypress");
 
