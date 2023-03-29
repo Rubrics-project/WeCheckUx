92a151897a47b9aa7b3a79c7b0256e890e0571d2
Shima Naderi <shima.naderii@gmail.com>
2023-03-29 13:08:39 +0200

    header tested

--- react/cypress/component/Header.cy.js
+++ react/cypress/component/Header.cy.js
@@ -5,13 +5,13 @@ import Header from "../../src/components/Header";
 describe('Accordion.cy.jsx', () => {
   it("renders correctly", () => {
     mount(<Header />);
-    cy.get("img[src='logo.svg']").should("exist"); // Check for the presence of logo image
+    cy.get("img[src='logo.svg']").should("exist"); 
   });
 
   it("displays the navigation menu", () => {
     cy.get("[data-testid='header-nav']").should("be.visible");
   });
-  
+
   it("displays the unlogged user icon by default", () => {
     cy.get("[data-testid='header-unlogged-icon']").should("be.visible");
     cy.get("[data-testid='header-logged-icon']").should("not.exist");
