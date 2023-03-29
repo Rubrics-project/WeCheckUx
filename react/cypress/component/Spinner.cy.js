c012b9f2d1d0bee94cad0f67a26477d939862c33
Shima Naderi <shima.naderii@gmail.com>
2023-03-29 14:44:04 +0200

    test Nav and spinner  added

---
+++ react/cypress/component/Spinner.cy.js
@@ -0,0 +1,10 @@
+import Spinner from "../../src/components/Spinner";
+
+describe('Accordion.cy.jsx', () => {
+    it('displays a spinner when loading', () => {
+        cy.visit('/')
+    
+        cy.get('div[role="status"]').should('be.visible') 
+        cy.get('span.sr-only').should('have.text', 'Loading...') 
+      })
+    })
