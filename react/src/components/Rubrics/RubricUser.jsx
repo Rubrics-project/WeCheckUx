26910fdb7e046f7a6dfaaaea529e1dbf7edc245e
mauisiri <irisleo.com@gmail.com>
2023-03-29 21:15:34 +0200

    perfect my rubrics

--- react/src/components/Rubrics/RubricUser.jsx
+++ react/src/components/Rubrics/RubricUser.jsx
@@ -68,8 +68,8 @@ export default function RubricUser({
   };
 
   return (
-    <div className="flex flex-col sm:flex-row-reverse sm:w-full ">
-      <div className="flex justify-center gap-4 mt-9 sm:flex-col-reverse sm:mx-3 sm:mt-4 sm:flex-wrap sm:justify-end">
+    <div className="flex flex-col sm:flex-row-reverse sm:w-full">
+      <div className="flex justify-center gap-2 mt-9 sm:flex-col-reverse sm:mx-3 sm:mt-4 sm:flex-wrap sm:justify-end">
         <ButtonSecondaryIconBlue
           text={"Editar"}
           src={edit}
@@ -82,15 +82,15 @@ export default function RubricUser({
           onClick={handleConfirmDelete}
         />
       </div>
-      <div className="w-full justify-items-stretch">
+      <div className=" w-full sm:ml-10">
         <Link to={`/mis-rubricas/${rubric_id}`}>
-          <div className="flex flex-col border rounded border-color-blue-p my-4 overflow-hidden sm:w-full">
-            <div className="flex gap-4 justify-between bg-color-grey-bg p-2">
-              <div className="flex bg-color-grey-bg p-2">
+          <div className="flex flex-col border rounded border-color-blue-p my-4 overflow-hidden sm:w-1/1">
+            <div className="grid grid-cols-7 gap-4 justify-between bg-color-grey-bg p-2">
+              <div className="col-span-6 flex bg-color-grey-bg p-2">
                 <img className="mx-2" src={icon} alt="Icono de doble check" />
-                <h4 className="font-latocustom font-bold text-lg ml-1">
+                <h2 className="font-latocustom font-bold text-lg ml-1">
                   {rubric_title}
-                </h4>
+                </h2>
               </div>
               <div
                 className="col-span-1 flex align-middle mr-3"
@@ -109,7 +109,7 @@ export default function RubricUser({
               <h4 className="font-opencustom text-xs text-color-grey-title ">
                 Proyecto:
               </h4>
-              <h2 className="font-latocustom font-bold text-lg pb-3">
+              <h2 className="font-latocustom font-bold text-sm pb-3">
                 {project_title}
               </h2>
             </div>
