6981e43cbc78aa2dec6982427c5389654f3f407e
mauisiri <irisleo.com@gmail.com>
2023-03-29 11:26:31 +0200

    done

--- react/src/components/buttons/ButtonPrimary.jsx
+++ react/src/components/buttons/ButtonPrimary.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function ButtonPrimary({ text }) {
   return (
     <button
-      className=" font-opencustom text-color-blue-p font-bold bg-color-grey-bg px-4 py-2 border-color-grey-border  border border-solid rounded-md"
+      className="font-opencustom text-color-blue-p font-bold bg-color-grey-bg px-4 py-2 border-color-grey-border  border border-solid rounded-md"
       type="submit"
     >
       {text}
