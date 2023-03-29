45c4c73ada129833f7a96544f31b5a6feb4c37b3
Shima Naderi <shima.naderii@gmail.com>
2023-03-29 09:12:19 +0200

    try

--- react/cypress.config.js
+++ react/cypress.config.js
@@ -11,3 +11,24 @@ module.exports = defineConfig({
     specPattern: 'cypress/components/**/*.spec.{js,ts,jsx,tsx}'
   },
 });
+
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
+
+
+
+
