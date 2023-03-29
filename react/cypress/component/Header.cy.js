860c0f054367a4c51d22d51cd2ba37bd851809a0
Shima Naderi <shima.naderii@gmail.com>
2023-03-29 13:07:36 +0200

    acc

--- react/cypress/component/Header.cy.js
+++ react/cypress/component/Header.cy.js
@@ -7,9 +7,11 @@ describe('Accordion.cy.jsx', () => {
     mount(<Header />);
     cy.get("img[src='logo.svg']").should("exist"); // Check for the presence of logo image
   });
+
   it("displays the navigation menu", () => {
     cy.get("[data-testid='header-nav']").should("be.visible");
   });
+  
   it("displays the unlogged user icon by default", () => {
     cy.get("[data-testid='header-unlogged-icon']").should("be.visible");
     cy.get("[data-testid='header-logged-icon']").should("not.exist");
