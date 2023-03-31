ddf43db7d83c84067746f4ae635e1e8bf4cb2dd0
mauisiri <irisleo.com@gmail.com>
2023-03-31 07:33:19 +0200

    information box

--- react/src/components/InformationBox.jsx
+++ react/src/components/InformationBox.jsx
@@ -3,7 +3,7 @@ import infoIcon from "../assets/infoCircle.svg";
 
 export default function InformationBox({ text }) {
   return (
-    <div className="p-3 flex bg-color-blue-light bg-opacity-20 w-full rounded mt-6">
+    <div className="flex justify-center p-3 bg-color-blue-light bg-opacity-20 w-full rounded mt-6">
       <img className="mr-3" src={infoIcon} alt="information icon" />
       <p className="font-opencustom text-xs text-color-blue-p ">{text}</p>
     </div>
